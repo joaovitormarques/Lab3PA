c4c0c53f49d21cb895230b5c9d71a4b2f8696a0b
joaovitormarques <joaovitormarques17@gmail.com>
2016-10-24 19:25:10 -0200

    Tree Searchs implemented

--- src/lab3pa/Lab3PA.java
+++ src/lab3pa/Lab3PA.java
@@ -16,6 +16,146 @@ import java.util.Map;
  */
 public class Lab3PA {
 
+    
+    //função que retorna o número truncado
+    public static double trunca(double y){
+        if(y - Math.floor(y) >= 0.5)
+            return Math.ceil(y);
+        return Math.floor(y);
+    }
+    
+    //classificador de arvore de decisão com base nas informações dos usuários
+    public static double classificacaoGeral(Map<Integer, Rating> avaliacoes, Map<Integer, User> usuarios, Movie filme) {
+        double nota = 0;
+        int n = 0;
+        double relevancia = 1, totalRelevancia = 0;
+        for (Rating avaliacao : avaliacoes.values()) {
+            if(avaliacao.movieId == filme.id){
+                relevancia = 1;
+                
+                //calcula a relevancia de acordo com o perfil do usuario
+                if(usuarios.get(avaliacao.userId).gender == 'M')
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).gender == 'F')
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).age == 1)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).age == 18)
+                    relevancia += 0.3;
+                if(usuarios.get(avaliacao.userId).age == 25)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).age == 35)
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).age == 45)
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).age == 50)
+                    relevancia -= 0.3;
+                if(usuarios.get(avaliacao.userId).age == 56)
+                    relevancia -= 0.3;
+                if(usuarios.get(avaliacao.userId).occupation == 0)
+                    relevancia -= 0;
+                if(usuarios.get(avaliacao.userId).occupation == 1)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 2)
+                    relevancia -= 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 3)
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 4)
+                    relevancia += 0.3;
+                if(usuarios.get(avaliacao.userId).occupation == 5)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 6)
+                    relevancia += 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 7)
+                    relevancia += 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 8)
+                    relevancia -= 0.3;
+                if(usuarios.get(avaliacao.userId).occupation == 9)
+                    relevancia -= 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 10)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 11)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 12)
+                    relevancia += 0.3;
+                if(usuarios.get(avaliacao.userId).occupation == 13)
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 14)
+                    relevancia -= 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 15)
+                    relevancia -= 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 16)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 17)
+                    relevancia += 0.3;
+                if(usuarios.get(avaliacao.userId).occupation == 18)
+                    relevancia += 0.1;
+                if(usuarios.get(avaliacao.userId).occupation == 19)
+                    relevancia -= 0.2;
+                if(usuarios.get(avaliacao.userId).occupation == 20)
+                    relevancia -= 0.1;
+                
+                totalRelevancia += relevancia;
+                nota += avaliacao.rating * relevancia;
+                n++;
+            }
+        }
+        if(n>0)
+            nota = nota/totalRelevancia; //é a média ponderada das avaliações que os usuários deram praquele filme
+        else return 0;
+        return trunca(nota);
+    }
+    
+    //classificador a priori(só classifica pelos gêneros do filme)
+    public static double classificacaoPriori(Movie filme){
+        double nota = 2.5;
+        String[] generos;
+        generos = filme.genre.split("[|]");
+        for(String palavra : generos){
+            if("Action".equals(palavra))
+                nota += 1.2;
+            if("Adventure".equals(palavra))
+                nota += 0.9;
+            if("Animation".equals(palavra))
+                nota += 0;
+            if("Children's".equals(palavra))
+                nota -= 0.5;
+            if("Comedy".equals(palavra))
+                nota += 0.9;
+            if("Crime".equals(palavra))
+                nota += 1.5;
+            if("Documentary".equals(palavra))
+                nota += 0.3;
+            if("Drama".equals(palavra))
+                nota += 0.6;
+            if("Fantasy".equals(palavra))
+                nota += 0;
+            if("Film-Noir".equals(palavra))
+                nota += 0.3;
+            if("Horror".equals(palavra))
+                nota -= 1.5;
+            if("Musical".equals(palavra))
+                nota -= 0.7;
+            if("Mystery".equals(palavra))
+                nota -= 0.9;
+            if("Romance".equals(palavra))
+                nota -= 0.7;
+            if("Sci-Fi".equals(palavra))
+                nota += 0.6;
+            if("Thriller".equals(palavra))
+                nota -= 1;
+            if("War".equals(palavra))
+                nota += 0.7;
+            if("Western".equals(palavra))
+                nota -= 0.7;
+        }
+        if(nota>5)
+            nota = 5;
+        if(nota <0)
+            nota = 0;
+        return trunca(nota);
+    }
+    
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
         //armazenando as informções dos usuários
@@ -28,14 +168,12 @@ public class Lab3PA {
 
         String temp = br.readLine();
         while (temp != null) {
-            //System.out.println(temp);//depois tira
             atributos = temp.split("::");
             auxUser = new User(Integer.parseInt(atributos[0]), atributos[1].charAt(0), Integer.parseInt(atributos[2]), Integer.parseInt(atributos[3]));
             usuarios.put(Integer.parseInt(atributos[0]), auxUser);
             temp = br.readLine();
         }
         file.close();
-        System.out.println(usuarios.get(4953).occupation);
 
         //armazenando as informações dos filmes
         FileReader file2 = new FileReader(new File("movies.dat"));
@@ -46,14 +184,12 @@ public class Lab3PA {
 
         String temp2 = br2.readLine();
         while (temp2 != null) {
-            //System.out.println(temp);//depois tira
             atributos2 = temp2.split("::");
             auxMovie = new Movie(Integer.parseInt(atributos2[0]), atributos2[1], atributos2[2]);
             filmes.put(Integer.parseInt(atributos2[0]), auxMovie);
             temp2 = br2.readLine();
         }
         file2.close();
-        System.out.println(filmes.get(1212).title);
 
         //armazenando as informações das avaliações
         FileReader file3 = new FileReader(new File("ratings.dat"));
@@ -64,14 +200,40 @@ public class Lab3PA {
 
         String temp3 = br3.readLine();
         while (temp3 != null) {
-            //System.out.println(temp);//depois tira
             atributos3 = temp3.split("::");
             auxRating = new Rating(Integer.parseInt(atributos3[0]), Integer.parseInt(atributos3[1]), Integer.parseInt(atributos3[2]), Integer.parseInt(atributos3[3]));
             avaliacoes.put(Integer.parseInt(atributos3[0])*10000 + Integer.parseInt(atributos3[1]), auxRating);
             temp3 = br3.readLine();
         }
         file3.close();
-        System.out.println(avaliacoes.get(40581792).timeStamp);
+        
+        
+        //classificação a priori dos meus 10 filmes:
+        /*
+        System.out.println(filmes.get(2).title + " " + classificacaoPriori(filmes.get(2)));
+        System.out.println(filmes.get(44).title + " " + classificacaoPriori(filmes.get(44)));
+        System.out.println(filmes.get(296).title + " " + classificacaoPriori(filmes.get(296)));
+        System.out.println(filmes.get(480).title + " " + classificacaoPriori(filmes.get(480)));
+        System.out.println(filmes.get(858).title + " " + classificacaoPriori(filmes.get(858)));
+        System.out.println(filmes.get(1101).title + " " + classificacaoPriori(filmes.get(1101)));
+        System.out.println(filmes.get(1205).title + " " + classificacaoPriori(filmes.get(1205)));
+        System.out.println(filmes.get(1580).title + " " + classificacaoPriori(filmes.get(1580)));
+        System.out.println(filmes.get(2571).title + " " + classificacaoPriori(filmes.get(2571)));
+        System.out.println(filmes.get(3404).title + " " + classificacaoPriori(filmes.get(3404)));
+        */
+        
+        //classificação baseado em árvore decisão dos meus 10 filmes
+        System.out.println(filmes.get(2).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(2)));
+        System.out.println(filmes.get(44).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(44)));
+        System.out.println(filmes.get(296).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(296)));
+        System.out.println(filmes.get(480).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(480)));
+        System.out.println(filmes.get(858).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(858)));
+        System.out.println(filmes.get(1101).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(1101)));
+        System.out.println(filmes.get(1205).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(1205)));
+        System.out.println(filmes.get(1580).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(1580)));
+        System.out.println(filmes.get(2571).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(2571)));
+        System.out.println(filmes.get(3404).title + " " + classificacaoGeral(avaliacoes, usuarios, filmes.get(3404)));
     }
+    
 
 }
